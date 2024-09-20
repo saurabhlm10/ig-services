08a5bcad6ee69edba4c901d8c719c118f0991a09
saurabhlm10 <saurabhlm10@gmail.com>
2024-09-21 01:14:51 +0530

    refactor: update successReturn

--- vulcan/src/utils/successReturn.util.ts
+++ vulcan/src/utils/successReturn.util.ts
@@ -1,8 +1,8 @@
 import { APIGatewayProxyResult } from 'aws-lambda';
 
-export function successReturn(message: string, data: any = {}): APIGatewayProxyResult {
+export function successReturn(message: string, data: object = {}, statusCode = 200): APIGatewayProxyResult {
     return {
-        statusCode: 200,
+        statusCode,
         body: JSON.stringify({
             message,
             data,
